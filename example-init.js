2b5be135abc85fa2f88d8f94f21153f49872ba38
Fernando Basso <fernandobasso.br@gmail.com>
2016-02-22 06:49:57 -0300

    Add example-init.js.

---
+++ example-init.js
@@ -0,0 +1,13 @@
+/**
+ * The literal object is the `config` object.
+ */
+player.init({
+    loop: 'all', // 'none', 'one', 'all'. Default: 'none'.
+    songs: [
+        //{url: 'https://upload.wikimedia.org/wikipedia/en/4/45/ACDC_-_Back_In_Black-sample', name: 'Black in Black'},
+        {url: 'songs/one', name: 'Amberdawn'},
+        {url: 'https://upload.wikimedia.org/wikipedia/en/4/4a/Bach_Prelude_Fugue_BWV_542', name: 'Prelude and Fugue'},
+        {url: 'songs/two', name: 'Fire In The Sky'},
+        {url: 'songs/three', name: 'Orange Blossom Special'}
+    ]
+});
